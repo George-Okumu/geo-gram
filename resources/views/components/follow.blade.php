567bf0dad77b73c341c3c6b487e8ea08bfa59050
George-Okumu <gokumu12@gmail.com>
2022-08-12 15:44:25 +0300

    telescope installation, to track my app interaction

---
+++ resources/views/components/follow.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <button onclick="alertMe()" class="bg-transparent w-full hover:bg-blue-500 text-gray-700 font-semibold hover:text-white py-1 px-4 border border-gray-600 hover:border-transparent rounded">
+        Follow
+    </button>
+</div>
+
+<script>
+    function alertMe(){
+        alert("Geogre");
+    }
+</script>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,45 +4,48 @@
             <img class="h-20 rounded-lg mr-4" src=" {{Auth::user()->profile->profileImage()}}" alt="Icon" />
         </div>
         <div class="mt-4">
-            <div class="w-full md:w-3/4 text-center">
-                <div class="text-left pt-3">
-                    <span class="text-bas text-gray-700 text-2xl">{{ Auth::user()->username }}</span>
+            <div class="text-left pt-3">
+                <span class="text-base text-gray-700 text-2xl">{{ Auth::user()->username }}</span>
 
-                </div>
+            </div>
+            <div class="grid grid-cols-3 gap-2">
                 <div class="pt-4">
                     <a href="/profile/{{Auth::user()->id}}/edit" class="bg-transparent hover:bg-blue-500 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-600 hover:border-transparent rounded">Edit Profile</a>
                 </div>
-
-                <div class="mt-4">
+                <div class="pt-4">
                     <span class="text-base font-semibold text-gray-700">
                         <a href="/posts/create" class="bg-transparent hover:bg-blue-500 text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-600 hover:border-transparent rounded">Create Post</a>
 
                     </span>
                 </div>
-
-                <div class="text-left pl-4 pt-3">
-                    <span class="text-base font-semibold text-gray-700 mr-2">
-                        <b>{{ Auth::user()->posts->count() }}</b> posts
-                    </span>
-                    <span class="text-base font-semibold text-gray-700 mr-2">
-                        <b>114</b> followers
-                    </span>
-                    <span class="text-base font-semibold text-gray-700">
-                        <b>200</b> following
-                    </span>
+                <div class="pt-2">
+                    <x-follow></x-follow>
                 </div>
+            </div>
 
-                <div class="text-left pl-4 pt-3">
-                    <span class="text-lg font-bold text-gray-700 mr-2">{{ Auth::user()->profile->title ?? "Not set"}}</span>
-                </div>
 
-                <div class="text-left pl-4 pt-3">
-                    <span class="text-lg font-bold text-gray-700 mr-2"><b>Description: </b>{{ Auth::user()->profile->description ?? "Not set"}}</span>
-                </div>
+            <div class="text-left pl-4 pt-3">
+                <span class="text-base font-semibold text-gray-700 mr-2">
+                    <b>{{ Auth::user()->posts->count() }}</b> posts
+                </span>
+                <span class="text-base font-semibold text-gray-700 mr-2">
+                    <b>114</b> followers
+                </span>
+                <span class="text-base font-semibold text-gray-700">
+                    <b>200</b> following
+                </span>
+            </div>
 
-                <div class="text-left pl-4 pt-3">
-                    <p class="text-base underline font-medium text-gray-700 mr-2"><a href="{{ Auth::user()->profile->linkUrl ?? "Link not set" }}">Portfolio</a></p>
-                </div>
+            <div class="text-left pl-4 pt-3">
+                <span class="text-lg font-bold text-gray-700 mr-2">{{ Auth::user()->profile->title ?? "Not set"}}</span>
+            </div>
+
+            <div class="text-left pl-4 pt-3">
+                <span class="text-lg font-bold text-gray-700 mr-2"><b>Description: </b>{{ Auth::user()->profile->description ?? "Not set"}}</span>
+            </div>
+
+            <div class="text-left pl-4 pt-3">
+                <p class="text-base underline font-medium text-gray-700 mr-2"><a href="{{ Auth::user()->profile->linkUrl ?? "Link not set" }}">Portfolio</a></p>
             </div>
         </div>
     </div>
